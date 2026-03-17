91cacaf8bfeb6c12cba65a3df5bf301001dfe4cf
alangt22 <alansilva2896@gmail.com>
2026-03-17 12:39:14 -0300

    fix: webview adicionadoao layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { SessionAuthProvider } from "@/providers/session-auth";
 import { Toaster } from "sonner";
 import NextTopLoader from "nextjs-toploader";
+import { WebViewWarning } from "./dashboard/_components/WebViewWarning";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,6 +34,7 @@ export default function RootLayout({
         <SessionAuthProvider>
           <NextTopLoader color="#0e55e1" height={6} showSpinner={false} />
           <Toaster position="top-right"/>
+          <WebViewWarning />
           {children}
         </SessionAuthProvider>
       </body>
